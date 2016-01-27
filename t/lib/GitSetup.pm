d9c33d7ee3016d38c5471da3e60dedb87d37dccc
Karen Etheridge <ether@cpan.org>
2016-01-26 18:22:21 -0800

    use Path::Tiny interface to check for rootdirs, to hopefully avoid needing the equality check

--- t/lib/GitSetup.pm
+++ t/lib/GitSetup.pm
@@ -20,17 +20,18 @@ sub no_git_tempdir
 
     {
         my $in_git;
-        my $rootdir = Path::Tiny->rootdir;
         my $dir = $tempdir;
         my $count = 0;
-        while ($dir ne $rootdir) {
+        while (not $dir->is_rootdir) {
+            # this should never happen.
+            do { diag "failed to detect that $dir is at the root?!"; last } if $dir eq $dir->parent;
+
             my $checkdir = path($dir, '.git');
             if (-d $checkdir) {
                 note "found $checkdir in $tempdir";
                 $in_git = 1;
                 last;
             }
-            last if $dir eq $dir->parent;
             $dir = $dir->parent;
         }
         continue {
