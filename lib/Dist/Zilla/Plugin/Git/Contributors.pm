2ac6e5aca87b351c0892d598cf1dee2b7605eda5
Karen Etheridge <ether@cpan.org>
2016-07-06 09:12:44 -0700

    elide timestamps from irc statements

--- lib/Dist/Zilla/Plugin/Git/Contributors.pm
+++ lib/Dist/Zilla/Plugin/Git/Contributors.pm
@@ -116,10 +116,10 @@ sub register_prereqs
     return if any { /^Dist::Zilla/ } keys %$all_prereqs;
 
     # if dynamic_config is set, the user gets another chance to read the file, via fallback code:
-    # 22:44 < haarg> eumm loads META, updates prereqs, and writes out MYMETA
-    # 22:44 < haarg> so in a working system, x_contributors will be included
-    # 22:45 < haarg> in a broken system, it will fail to load META, regenerate it from parameters including META_ADD/MERGE, then write it out
-    # 22:46 < haarg> so if there isn't any utf8 data in the parameters given to EUMM, it will produce a file that can be read by a "bad" JSON::PP
+    # < haarg> eumm loads META, updates prereqs, and writes out MYMETA
+    # < haarg> so in a working system, x_contributors will be included
+    # < haarg> in a broken system, it will fail to load META, regenerate it from parameters including META_ADD/MERGE, then write it out
+    # < haarg> so if there isn't any utf8 data in the parameters given to EUMM, it will produce a file that can be read by a "bad" JSON::PP
     return if $self->zilla->distmeta->{dynamic_config};
 
     # see https://github.com/makamaka/JSON-PP/pull/9 for for details
