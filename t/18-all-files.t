karenetheridge/Dist-Zilla-Plugin-Git-Contributors

commit 4bba482fec1b650f9a443ac7d20523a772b8d05f

fix path used in 'git log' to default to the repository root, rather than using the current directory

lib/Dist/Zilla/Plugin/Git/Contributors.pm
@@ -39,7 +39,7 @@ has order_by => (
 has paths => (
     isa => 'ArrayRef[Str]',
     lazy => 1,
-    default => sub { [] },
+    default => sub { [ shift->zilla->root . '' ] },
     traits => ['Array'],
     handles => { paths => 'elements' },
 );
@@ -49,11 +49,13 @@ around dump_config => sub
     my ($orig, $self) = @_;
     my $config = $self->$orig;
 
+    my @paths = $self->paths;
+
     $config->{+__PACKAGE__} = {
         include_authors => $self->include_authors,
         include_releaser  => $self->include_releaser,
         order_by => $self->order_by,
-        paths => [ $self->paths ],
+        paths => [ @paths == 1 ? () : @paths ],
     };
 
     return $config;
@@ -84,9 +86,6 @@ sub _contributors
 
     return [] if not $in_repo;
 
-    my @paths = $self->paths;
-    unshift @paths, '--' if @paths;
-
     my @data = try {
         $self->_git(shortlog =>
             {
@@ -94,7 +93,7 @@ sub _contributors
                 summary => 1,
                 $self->order_by eq 'commits' ? ( numbered => 1 ) : (),
             },
-            'HEAD', @paths,
+            'HEAD', '--', $self->paths,
         );
     };
 

commit 5a92088a1020ce1262fbef42df164d9df07fba41

do not omit empty (file-less) commits from consideration
These are often used to give attribution to people who had patches
submitted with incorrect attribution applied to the commits, or to give thanks
to people who provided help in other ways.

This was regressed in commit 4bba482f, which started always passing a path(s)
to the 'git shortlog' command -- with no paths provided, all commits from the
repository are considered.

lib/Dist/Zilla/Plugin/Git/Contributors.pm
@@ -42,7 +42,7 @@ has order_by => (
 has paths => (
     isa => 'ArrayRef[Str]',
     lazy => 1,
-    default => sub { [ shift->zilla->root . '' ] },
+    default => sub { [] },
     traits => ['Array'],
     handles => { paths => 'elements' },
 );

t/01-basic.t
@@ -85,7 +85,7 @@ cmp_deeply(
                             include_authors => 0,
                             include_releaser => 1,
                             order_by => 'name',
-                            paths => ['.'],
+                            paths => [],
                             'git --version' => ignore,
                         },
                     },

t/12-mailmap-files.t
@@ -65,7 +65,7 @@ use GitSetup;
                                 include_authors => 0,
                                 include_releaser => 1,
                                 order_by => 'name',
-                                paths => ['.'],
+                                paths => [],
                                 'git --version' => ignore,
                             },
                         },

t/14-extract-author.t
@@ -68,7 +68,7 @@ cmp_deeply(
                             include_authors => 0,
                             include_releaser => 1,
                             order_by => 'name',
-                            paths => ['.'],
+                            paths => [],
                             'git --version' => ignore,
                         },
                     },

t/18-all-files.t
@@ -25,7 +25,7 @@ my $tzil = Builder->from_config(
                 },
                 [ GatherDir => ],
                 [ MetaConfig => ],
-                [ 'Git::Contributors' => { remove => 'foo' } ],
+                [ 'Git::Contributors' ],
             ),
             path(qw(source lib Foo.pm)) => "package Foo;\n1;\n",
         },
@@ -41,9 +41,19 @@ $changes->spew("Release history for my dist\n\n");
 $git->add('Changes');
 $git->commit({ message => 'first commit', author => 'Hey Jude <jude@example.org>' });
 
-$changes->append("- a changelog entry\n");
-$git->add('Changes');
-$git->commit({ message => 'second commit', author => 'Foo Bar <foo@bar.com>' });
+my $module = $root->child('lib', 'Foo.pm');
+$module->parent->mkpath;
+$module->append("'ohhai'\n");
+$git->add($module->stringify);
+$git->commit({ message => 'second commit', author => 'Anon Y. Moose <anon@null.com>' });
+
+my $test = $root->child('t', 'foo.t');
+$test->parent->mkpath;
+$test->spew("use Test::More tests => 1;\npass('ohhai');\n");
+$git->add($test->stringify);
+$git->commit({ message => 'third commit', author => 'Foo Bar <foo@bar.com>' });
+
+$git->commit({ message => 'empty commit', author => 'A Silent Contributor <silent@dude.com>', 'allow-empty' => 1 });
 
 $tzil->chrome->logger->set_debug(1);
 
@@ -57,6 +67,8 @@ cmp_deeply(
     $tzil->distmeta,
     superhashof({
         x_contributors => [
+            'A Silent Contributor <silent@dude.com>',
+            'Foo Bar <foo@bar.com>',
             'Hey Jude <jude@example.org>',
         ],
         x_Dist_Zilla => superhashof({
@@ -64,14 +76,9 @@ cmp_deeply(
                 {
                     class => 'Dist::Zilla::Plugin::Git::Contributors',
                     config => {
-                        'Dist::Zilla::Plugin::Git::Contributors' => {
-                            include_authors => 0,
-                            include_releaser => 1,
-                            order_by => 'name',
-                            paths => [],
-                            remove => '...',
-                            'git --version' => ignore,
-                        },
+                        'Dist::Zilla::Plugin::Git::Contributors' => superhashof({
+                            paths => [ ],
+                        }),
                     },
                     name => 'Git::Contributors',
                     version => Dist::Zilla::Plugin::Git::Contributors->VERSION,
@@ -79,7 +86,7 @@ cmp_deeply(
             ),
         }),
     }),
-    'contributor names are extracted, with requested entries removed',
+    'contributor names are extracted, even from path-less commits in the repository',
 ) or diag 'got distmeta: ', explain $tzil->distmeta;
 
 diag 'got log messages: ', explain $tzil->log_messages