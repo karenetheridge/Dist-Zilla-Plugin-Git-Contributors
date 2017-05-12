7e1af872aaa44d4a2540e879ed4785b943cf6fe8
Karen Etheridge <ether@cpan.org>
2017-05-12 17:26:14 +0200

    remove spaces from configs in metadata
    A YAML line like
    
        "git --version": 1.2.3
    
    is generated in newer versions of the YAML encoder, but these quotes are
    unexpected in older versions of the decoder (CPAN::Meta::YAML 0.10 ->
    0.11, i.e. YAML::Tiny 1.55 -> 1.60, but bisects to commit
    73ea4027, i.e. YAML::Tiny 1.57 containing the changed behaviour).
    Consequently Test::CPAN::Meta fails in older perls when checking a
    META.yml that was generated in a modern perl.  This *probably* hasn't
    actually caused problems in the wild, since the toolchain knows how to
    fall back to makefile args encoded in the Makefile directly (i.e. to get
    configure_requires prerequisites), but it's still best to just avoid the
    situation.

--- lib/Dist/Zilla/Plugin/Git/Contributors.pm
+++ lib/Dist/Zilla/Plugin/Git/Contributors.pm
@@ -71,7 +71,7 @@ around dump_config => sub
                      ($dist_root->subsumes($p) ? $p->relative($dist_root) : $p)->stringify
                    } $self->paths ],
         $self->remove ? ( remove => '...' ) : (),
-        'git --version' => $self->_git('version'),
+        git_version => $self->_git('version'),
         blessed($self) ne __PACKAGE__ ? ( version => $VERSION ) : (),
     };
 
