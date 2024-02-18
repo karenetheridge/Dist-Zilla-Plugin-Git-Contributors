2c91a1b65f8ad21e769c52b5246b693078e4d13d
Karen Etheridge <ether@cpan.org>
2024-02-18 15:50:48 -0800

    increment $VERSION after 0.037 release

--- lib/Dist/Zilla/Plugin/Git/Contributors.pm
+++ lib/Dist/Zilla/Plugin/Git/Contributors.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::Git::Contributors;
 # ABSTRACT: Add contributor names from git to your distribution
 # KEYWORDS: plugin distribution metadata git contributors authors commits
 
-our $VERSION = '0.037';
+our $VERSION = '0.038';
 
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider',
